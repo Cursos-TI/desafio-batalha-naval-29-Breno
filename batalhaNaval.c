dee520a243a6d4e68993ed32af7b2ab2ee210f51
29-Breno <brenofreis69@gmail.com>
2025-05-23 20:40:08 +0000

    Nivel Aventureri Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    int tabuleiro[10][10] = {0}; //inicializa matriz com 0 (água)
+    int tabuleiro[10][10] = {0}; // Inicializa matriz com 0 (água)
 
     // Posiciona navio horizontal de tamanho 3 na linha 2, coluna 3
-
-    tabuleiro[2][3] = 3; //linha 2 e coluna 3
-    tabuleiro[2][4] = 3; //linha 2 e coluna 4
+    tabuleiro[2][3] = 3;
+    tabuleiro[2][4] = 3;
     tabuleiro[2][5] = 3;
-    //usa o nome tabuleiro pois,precisa ser o mesmo para se referir à mesma matriz.
 
-    // Posiciona navio vertical de tamanho 3 na linha 5, coluna 5
-
+    // Posiciona navio vertical de tamanho 3 na coluna 5, linhas 5 a 7
     tabuleiro[5][5] = 3;
-    tabuleiro[6][5] = 3;//linha 6 e coluna 5
-    tabuleiro[7][5] = 3;//linha 7 e coluna 5
+    tabuleiro[6][5] = 3;
+    tabuleiro[7][5] = 3;
+
+    // Posiciona navio diagonal (diagonal principal): tabuleiro[i][i]
+    tabuleiro[0][0] = 3;//A linha e coluna têm mesmo índice. Sendo linha 0 e coluna 0 e assim por diante
+    tabuleiro[1][1] = 3;
+    tabuleiro[2][2] = 3;
+
+    // Posiciona navio diagonal (diagonal secundária): tabuleiro[i][9 - i]
+    tabuleiro[0][9] = 3;//A linha é L,a coluna é o contrário dela:9 - L. Sendop assim L++ e C--
+    tabuleiro[1][8] = 3;
+    tabuleiro[2][7] = 3;
 
     // Exibe o tabuleiro
-    //L representa a linha e C representa a coluna.
     for (int L = 0; L < 10; L++) {
         for (int C = 0; C < 10; C++) {
-            printf("%d ", tabuleiro[L][C]); //imprime o valor da posição [L][C] da matriz
+            printf("%d ", tabuleiro[L][C]);
         }
         printf("\n");
     }
